eca9b5ae6b6aed59946cecb56bad137a0ed405f8
Yixin Sun <sun955967@gmail.com>
2016-02-13 16:43:26 -0500

    Initial Commit

---
+++ FindMyFriendsTests/FindMyFriendsTests.swift
@@ -0,0 +1,36 @@
+//
+//  FindMyFriendsTests.swift
+//  FindMyFriendsTests
+//
+//  Created by Yixin Sun on 2/13/16.
+//  Copyright © 2016 Yixin Sun. All rights reserved.
+//
+
+import XCTest
+@testable import FindMyFriends
+
+class FindMyFriendsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
