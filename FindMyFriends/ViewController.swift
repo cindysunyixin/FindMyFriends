4ac817dfffd713415e96d21c61d533d99a1a6235
Yixin Sun <sun955967@gmail.com>
2016-02-13 21:39:20 -0500

    First commit

--- FindMyFriends/ViewController.swift
+++ FindMyFriends/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
+    @IBOutlet weak var MapView: MKMapView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.delegate = self
+        if CLLocationManager.locationServicesEnabled() {
+            if CLLocationManager.authorizationStatus() == .NotDetermined {
+                if self.locationManager.respondsToSelector("RequestWhenInUseAuthrorization") {
+                    self.locationManager.requestAlwaysAuthorization()
+                }
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(manager:CLLocationManager, didChangeAuthrorizationStatus status: 
 
 
 }
